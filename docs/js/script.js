9c6e1b6b6aa41cfb01df5fda7c5d1115d5e982ec
7hUd41 <wontread@shittymail.com>
2024-05-04 19:33:23 +0200

    init

---
+++ docs/js/script.js
@@ -0,0 +1,4 @@
+document$.subscribe(function() {
+    console.log("Initialize third-party libraries here")
+    // https://squidfunk.github.io/mkdocs-material/customization/#additional-javascript
+  })
